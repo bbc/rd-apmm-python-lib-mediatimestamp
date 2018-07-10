ed847412ac03ebf0ee80129b61072d25f25a573c
James P. Weaver <james.barrett@bbc.co.uk>
2018-07-10 17:54:17 +0100

    1.0.0-dev3: Version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,5 @@
 - Initial version, porting timestamp components from nmos-common v.0.6.0
 - Has Jenkinsfile for automation
 - Has TimeRange
+- TimeOffset.__eq__ doesn't raise exceptions when called with a
+  different type
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import os
 
 # Basic metadata
 name = 'mediatimestamp'
-version = '1.0.0-dev2'
+version = '1.0.0-dev3'
 description = 'A timestamp library for high precision nanosecond timestamps'
 url = 'https://github.com/bbc/rd-apmm-python-lib-mediatimestamp'
 author = 'James P. Weaver'
